7c83bed68139ef6b0a5252b04527dd3ee3b878a4
alinaast <55184585+alinaast@users.noreply.github.com>
2019-10-23 11:09:32 +0300

    Add files via upload

---
+++ Lab2.4.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int x1, x2, y1, y2, a, b, p, s, d;
+	cin >> x1 >> x2 >> y1 >> y2;
+	a = abs(x1 - x2);
+	b = abs(y1 - y2);
+	d = sqrt(a * a + b * b);
+	p = 2 * a + 2 * b;;
+	s = 8 * d * d;
+	cout << p << endl << s;
+	return 0;
+}
